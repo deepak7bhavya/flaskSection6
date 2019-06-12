d6084a81077281a8411a23a87a79b81e589dd5e3
Deepak Kumar <deepak986698@gmail.com>
2019-06-12 12:32:29 +0530

    Adding Heroku FIles to run

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template,jsonify
+from flask import Flask, render_template
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 from sendmail import sendMail
